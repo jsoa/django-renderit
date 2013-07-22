35923e62144a12971cd3e987ea7dd8d498cce774
Jose Soa <josefsoares@gmail.com>
2013-07-22 08:28:58 -0400

    [1.0] Reworked the template tags

--- renderit/templatetags/renderit_tags.py
+++ renderit/templatetags/renderit_tags.py
@@ -1,26 +1,91 @@
-from django.db.models import get_model, Model
-from django.template import Library, Node, TemplateSyntaxError, Variable, VariableDoesNotExist
-from django.utils.translation import ugettext as _
-from django.template.defaultfilters import slugify
-from django.contrib.contenttypes.models import ContentType
-from django.template.loader import select_template
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from django.db.models import Model
+from django.template import Library, Node, TemplateSyntaxError, Variable
 from django.template import Context
+from django.template.loader import select_template
 
 register = Library()
 
-from renderit.settings import CONCATINATION_STRING, ROOT_TEMPLATE_PATH
+from ..settings import CONCATINATION_STRING, ROOT_TEMPLATE_PATH
+
+
+def generate_type_string(obj, concat=CONCATINATION_STRING):
+    """Retreive the type, either a django content type or just the object type.
+
+    """
+    type_str = ""
+    if isinstance(obj, Model):
+        type_str = "%s%s%s" % (
+            obj._meta.app_label, concat, obj._meta.module_name)
+    else:
+        type_str = obj.__class__.__name__
+
+    return type_str.lower()
+
+
+def generate_template_list(type_string, args=None, prefix=None, group=None,
+                           concat=CONCATINATION_STRING,
+                           default_tmpl='default',
+                           default_tmpl_path=ROOT_TEMPLATE_PATH):
+    """Generate a template list using supplied arguments.
+
+    `type_string` - the type of object
+    `args` - a list of suffixes append to the `type_string`
+    `prefix` - a string that is prefixed to the `type_string`
+    `group` - a string of the group, a.k.a. folder
+    `concat` - a string used when append the strings together
+    `default_tmpl` - the default template name
+    `default_tmpl_path` - root path where to look for the generated templates
+
+    """
+    args, prefix, group = args or [], prefix or '', group or ''
+    template_list = []
+
+    arg_list = list(args)
+    # Insert to the object type string to the arg list
+    arg_list.insert(0, type_string)
+    # Build the default arg list without group or prefix
+    argstr_list = [concat.join(arg_list[0:x]) for x in \
+                    range(len(arg_list), 0, -1)]
+
+    # Using the arg_str_list apply the prefix (if one is supplied)
+    prefix_list = ['%s%s%s' % (prefix, concat, x) for x in \
+                   argstr_list if prefix]
+
+    # Start building the template list starting with group, prefix and args,
+    # then just group and args.
+    template_list.extend(['%s/%s' % (group, x) for x in prefix_list if group])
+    template_list.extend(['%s/%s' % (group, x) for x in argstr_list if group])
+
+    # Add the prefixed_list and arg_str_list (without group)
+    template_list.extend(prefix_list)
+    template_list.extend(argstr_list)
+
+    # Add the default template
+    template_list.append(default_tmpl)
+
+    # Rebuild the template list adding the path and extension
+    template_list = ['%s/%s.html' % (
+        default_tmpl_path, x) for x in template_list]
+
+    return template_list
+
 
 def resolve_variable(value, context, none_on_fail=False):
-    """
-    A wrapper function to resolve template variables.
+    """A wrapper function to resolve template variables.
+
     Will return None if [none_on_fail] is [True], otherwise
     it will return [value]
+
     """
     try:
         return Variable(value).resolve(context)
     except Exception:
-        if none_on_fail: return None
+        if none_on_fail:
+            return None
         return value
+
 
 class RenderItNode(Node):
     def __init__(self, obj, path_args, extra_kwgs, varname):
@@ -67,7 +132,8 @@ def do_renderit(parser, token):
     argc = len(argv)
 
     if argc < 1:
-        raise TemplateSyntaxError, '%s tag requires at least one argument' % argv[0]
+        raise TemplateSyntaxError(
+            '%s tag requires at least one argument' % argv[0])
 
     # first item is the object to be rendered
     obj = argv[1]
@@ -77,7 +143,8 @@ def do_renderit(parser, token):
     # get the index from 'with' to 'as' or the total length of arguments
     e = 'with' in argv and argv.index('with') or a
 
-    # the path args are from the object to either 'with', 'as' or length of arguments
+    # the path args are from the object to either 'with', 'as' or
+    # length of arguments
     path_args = argv[2: e]
     # extra args are from 'with' to 'as' or to length of arguments
     extra_args = argv[e+1: a]
@@ -85,7 +152,8 @@ def do_renderit(parser, token):
     varname = argv[a:] and argv[a+1] or None
 
     # split the extra args by '=' and make a doct
-    extra_kwgs = dict([(x.split("=")[0],x.split("=")[1]) for x in extra_args if '=' in x])
+    extra_kwgs = dict([(x.split("=")[0],x.split("=")[1]) for x in \
+                       extra_args if '=' in x])
 
     return RenderItNode(obj, path_args, extra_kwgs, varname)
 
@@ -102,47 +170,15 @@ def render_obj(obj, *args, **kwargs):
     context = kwargs.pop("context", False)
     extra_context = kwargs.pop("extra_context", {})
 
-    # Retreive the type, either a django content type or just the object type
-    type_string = ""
-    if isinstance(obj, Model):
-        ctype = ContentType.objects.get_for_model(obj)
-        type_string = "%s%s%s" % (ctype.app_label.lower(), concat, ctype.model.lower())
-    else:
-        type_string = obj.__class__.__name__
+    type_str = generate_type_string(obj, concat)
 
-    template_list = []
-    # The default render template path
-    default_tmpl_path = ROOT_TEMPLATE_PATH
-
-    default_tmpl = "default"
-
-    arg_list = list(args)
-    # Insert to the object type string to the arg list
-    arg_list.insert(0, type_string)
-    # Build the default arg list without group or prefix
-    arg_str_list = [concat.join(arg_list[0:x]) for x in range(len(arg_list), 0, -1)]
-
-    # Using the arg_str_list apply the prefix (if one is supplied)
-    prefixed_list = ['%s%s%s' % (prefix, concat, x) for x in arg_str_list if prefix]
-
-    # Start building the template list starting with group, prefix and args,
-    # then just group and args.
-    template_list.extend(['%s/%s' % (group, x) for x in prefixed_list if group])
-    template_list.extend(['%s/%s' % (group, x) for x in arg_str_list if group])
-
-    # Add the prefixed_list and arg_str_list (without group)
-    template_list.extend(prefixed_list)
-    template_list.extend(arg_str_list)
-
-    # Add the default template
-    template_list.append(default_tmpl)
-
-    # Rebuild the template list adding the path and extension
-    template_list = ['%s/%s.html' % (default_tmpl_path, x) for x in template_list]
+    # Generate the template list
+    template_list = generate_template_list(
+        type_str, args, prefix, group, concat=concat)
 
     # Select the template
-    t = select_template(template_list)
-    if not t:
+    tmpl = select_template(template_list)
+    if not tmpl:
         return None
 
     # Add the context
@@ -152,4 +188,4 @@ def render_obj(obj, *args, **kwargs):
         ret_context.update(extra_context)
 
     # return the rendered content
-    return t.render(ret_context)
+    return tmpl.render(ret_context)
