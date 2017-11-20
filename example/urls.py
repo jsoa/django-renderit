faf010bb2013d78a06d8e30a5f0422e587d0aaea
Jose Soares <jose@linux.com>
2017-11-20 08:49:03 -0500

    [1.2] Another fix for example urls

--- example/urls.py
+++ example/urls.py
@@ -1,7 +1,7 @@
 try:
     from django.conf.urls.defaults import patterns, url
 except (ImportError, ):
-    from django.conf.urls import patterns, url
+    from django.conf.urls import url
 
 
 from django.views.generic.simple import direct_to_template
@@ -18,22 +18,25 @@ string_value = "this is something"
 
 unicode_value = u"This is something else"
 
+
 class c(object):
     var1 = 123
     var2 = "custom"
 
-context = {'main_user': main_user,
-           'int_value': int_value,
-           'float_value': float_value,
-           'string_value': string_value,
-           'unicode_value': unicode_value,
-           'custom_class': c}
+
+context = {
+    'main_user': main_user,
+    'int_value': int_value,
+    'float_value': float_value,
+    'string_value': string_value,
+    'unicode_value': unicode_value,
+    'custom_class': c}
 
 context.update(get_or_create_example())
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$',
         direct_to_template,
         {'template': 'index.html',
          'extra_context': context}),
-)
+]
