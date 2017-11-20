f390b12372613ef41ae4a704805ddb01c2866087
Jose Soares <jose@linux.com>
2017-11-20 08:42:31 -0500

    [1.2] Fixed 1.11 issue

--- example/urls.py
+++ example/urls.py
@@ -1,4 +1,7 @@
-from django.conf.urls.defaults import patterns, url
+try:
+    from django.conf.urls.defaults import patterns, url
+except (ImportError, ):
+    from django.conf.urls import patterns, url
 
 
 from django.views.generic.simple import direct_to_template
